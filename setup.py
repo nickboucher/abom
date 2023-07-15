e6ec74b3a4f59d1809d69135700cfa9cfffa723e
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2023-07-15 21:05:17 +0100

    Rename module to abom

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='abom-clang',
+    name='abom',
     version='0.1',
     author='Nicholas Boucher',
     author_email='nicholas.boucher@cl.cam.ac.uk',
-    description='Automatic Bill of Materials for clang',
+    description='Automatic Bill of Materials',
     keywords='abom, bom, clang',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/nickboucher/abom-clang',
-    package_dir={'': 'abom-clang'},
-    packages=setuptools.find_packages(where='abom-clang'),
+    url='https://github.com/nickboucher/abom',
+    package_dir={'': 'abom'},
+    packages=setuptools.find_packages(where='abom'),
     python_requires='>=3.6',
     install_requires=[
             'termcolor',
