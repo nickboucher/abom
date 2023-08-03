f4afc41cea64eff2629d5e82d8bd0dbbe9cb50fb
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2023-08-03 02:16:18 +0100

    Update plots

--- src/utils/tuning.py
+++ src/utils/tuning.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib import colormaps
 import numpy as np
 from abom import CompressedBloomFilter
 from hashlib import sha3_256
@@ -90,18 +92,28 @@ def add_line(ax, xpos, ypos):
     line.set_clip_on(False)
     ax.add_line(line)
 
-def label_group_bar(ax, data):
+def label_group_bar(ax, data, colormap='inferno'):
     groups = mk_groups(data)
     xy = groups.pop()
     x, y = zip(*xy)
     ly = len(y)
     xticks = range(1, ly + 1)
 
-    ax.bar(xticks, y, align='center')
+    bars = ax.bar(xticks, y, align='center')
     ax.set_xticks(xticks)
     ax.set_xticklabels(x, rotation = 45, size=8)
     ax.set_xlim(.5, ly + .5)
     ax.yaxis.grid(True)
+
+    colors = colormaps[colormap]
+    n = 0
+    j = 0
+    for _, span in groups[0]:
+        color = colors(j / len(groups[0]))
+        for i in range(n, n + span):
+            bars[i].set_color(color)
+        n += span
+        j += 1
 
     scale = 1. / ly
     for pos in range(ly + 1):
@@ -117,7 +129,12 @@ def label_group_bar(ax, data):
             pos += rpos
         add_line(ax, pos * scale, ypos+.05)
         ypos -= .1
-    
+
+def f_max(f: float) -> str:
+    if f == 2**-14:
+        return '2^{-14}'
+    else:
+        return f'{f:f}'
 
 def main() -> None:
     parser = ArgumentParser(prog='ABOM Tuning', description='Tune ABOM parameters via experiments.')
@@ -130,11 +147,12 @@ def main() -> None:
     parser_experiment.add_argument('-o', '--out', type=str, default='tuning.json', help='Output JSON file.')
 
     parser_graph = subparsers.add_parser('graph', help='Graph tuning experimental results.')
-    parser_graph.add_argument('-f', '--max-f', type=float, default=1/(2**14), help='Maximum false positive rate to graph.')
+    parser_graph.add_argument('-f', '--max-f', type=float, default=2**-14, help='Maximum false positive rate to graph.')
     parser_graph.add_argument('-n', '--min-n', type=int, default=1000, help='Minimum number of items inserted for target false positive rate.')
     parser_graph.add_argument('-t', '--top', type=int, default=5, help='Number of optimal Bloom filter configurations to output in table.')
     parser_graph.add_argument('-i', '--input', type=str, default='tuning.json', help='Input JSON file.')
     parser_graph.add_argument('-d', '--out-dir', type=str, default='graphs', help='Directory to hold output graphs.')
+    parser_graph.add_argument('-m', '--include-2m', nargs='*', type=int, default=[14, 16, 20, 24], help='The values of 2^m to include on the combined 4D plots.')
 
     args = parser.parse_args()
 
@@ -172,16 +190,19 @@ def graph(args: Namespace) -> None:
     graph_4D(stats, args.out_dir, args.max_f)
     optimals = graph_optimals(stats, args.out_dir, args.max_f, args.min_n)
     optimals_table(optimals, args.out_dir, args.max_f, args.min_n, args.top)
+    graph_combined_4D(stats, args.out_dir, args.max_f, args.include_2m)
 
-def graph_4D(stats: list[BFStats], out_dir: str, max_f: float) -> None:
-    makedirs(join(out_dir, '4D'), exist_ok=True)
+def graph_4D(stats: list[BFStats], out_dir: str, max_f: float, fig: None|Figure = None) -> None:
+    if not fig:
+        makedirs(join(out_dir, '4D'), exist_ok=True)
     M = defaultdict(list)
     for x in stats:
         M[x.m].append(x)
 
-    for m, X in M.items():
-        filename = join(out_dir, f'4D/m-{m}.png')
-        print(f'Generating {filename}...')
+    for i, (m, X) in enumerate(M.items()):
+        if not fig:
+            filename = join(out_dir, f'4D/m-{int(log(m, 2))}.pdf')
+            print(f'Generating {filename}...')
 
         F = dict()
         K = defaultdict(list)
@@ -200,7 +221,10 @@ def graph_4D(stats: list[BFStats], out_dir: str, max_f: float) -> None:
             N = sorted(k, key=lambda x: x.n)
             return [(N[0].n, 0.)] + [(n.n, n.z_bytes) for n in N] + [(n_max, N[-1].z_bytes), (n_max, 0.)]
         
-        ax = plt.figure().add_subplot(projection='3d',computed_zorder=False)
+        if fig:
+            ax = fig.add_subplot(1, len(M.keys()), i+1, projection='3d', computed_zorder=False)
+        else:
+            ax = plt.figure().add_subplot(projection='3d', computed_zorder=False)
         verts = [polygon_under_graph(k) for k in K.values()]
         zs = list(K.keys())
 
@@ -209,7 +233,7 @@ def graph_4D(stats: list[BFStats], out_dir: str, max_f: float) -> None:
         ax.add_collection3d(poly, zs=zs, zdir='y')
 
         for f in F.values():
-            ax.scatter(f.n, f.k, f.z_bytes, c='darkorange', edgecolors='black', linewidths=.25, s=6, zorder=1, label='False Positive\nRate = ' + f'{max_f}'.lstrip('0').rstrip('0'))
+            ax.scatter(f.n, f.k, f.z_bytes, c='darkorange', edgecolors='black', linewidths=.25, s=6, zorder=1, label='False Positive\nRate = $' + f_max(max_f).lstrip('0').rstrip('0') + '$')
 
         ax.set(xlim=(0, n_max), ylim=(1, k_max), zlim=(0, z_bytes_max),
                xlabel='n\nElements Inserted', ylabel='k\nHash Functions', zlabel='z\nCompressed Size\n(bytes)')
@@ -220,11 +244,29 @@ def graph_4D(stats: list[BFStats], out_dir: str, max_f: float) -> None:
 
         handles, labels = plt.gca().get_legend_handles_labels()
         dedup = dict(zip(labels, handles))
-        ax.legend(dedup.values(), dedup.keys(), fontsize="9")
+        if fig:
+            plt.xticks(rotation=15)
+            ax.legend(dedup.values(), map(lambda x: x.replace('\n', ' '), dedup.keys()), loc='upper center', fontsize="7")
+        else:
+            ax.legend(dedup.values(), dedup.keys(), fontsize="9")
 
-        plt.title(f'$2^{{{log(m, 2):.0f}}}$\nBloom Filter Bits (m)')
-        plt.savefig(filename, dpi=300)
-        plt.close()
+        ax.set_title(f'$2^{{{log(m, 2):.0f}}}$\nBloom Filter Bits (m)')
+        if not fig:
+            plt.savefig(filename)
+            plt.close()
+
+def graph_combined_4D(stats: list[BFStats], out_dir: str, max_f: float, include_2m: list[int]) -> None:
+    stats2 = []
+    include_2m = list(map(lambda x: 2**x, include_2m))
+    for stat in stats:
+        if stat.m in include_2m:
+            stats2.append(stat)
+    fig = plt.figure(figsize=plt.figaspect(1/len(include_2m)))
+    graph_4D(stats2, out_dir, max_f, fig)
+    plt.suptitle('$m,n,k,z$ Relationship\nfor Selected Values', fontsize=16, y=1.05)
+    plt.subplots_adjust(wspace=.6)
+    plt.savefig(join(out_dir, 'combined_4D.pdf'), bbox_inches='tight', pad_inches=0.55)
+    plt.close() 
 
 def graph_optimals(stats: list[BFStats], out_dir: str, max_f: float, min_n: int) -> defaultdict[defaultdict[float]]:
     M = defaultdict(lambda: defaultdict(list))
@@ -247,9 +289,10 @@ def graph_optimals(stats: list[BFStats], out_dir: str, max_f: float, min_n: int)
     ax = fig.add_subplot(1,1,1)
     label_group_bar(ax, optimals)
     fig.subplots_adjust(bottom=0.3)
-    plt.title(f'Optimal $(m,n,k)$\nfor $f\leq{str(max_f).lstrip("0")}$ and $n\geq{min_n}$')
-    plt.ylabel('Compressed Size (bytes)')
-    fig.savefig(join(out_dir,'optimals.png'), dpi=300)
+    plt.title(f'$m,n,k$\nfor $f\leq{f_max(max_f).lstrip("0")}$ and $n\geq{min_n}$', fontsize=16, y=1.05)
+    plt.ylabel('Compressed Size (bytes)\n$z$')
+    plt.xlabel('$m, (n,k)$\nBloom Filter Bits, (Elements, Hash Functions)', labelpad=25)
+    fig.savefig(join(out_dir,'optimals.pdf'), bbox_inches='tight')
     plt.close()
     return optimals
 
@@ -266,8 +309,8 @@ def optimals_table(optimals: defaultdict[defaultdict[float]], out_dir: str, max_
     fig.set_figheight(t/5)
     ax.axis('off')
     ax.table(cellText=rows[:t], colLabels=('z', 'm', 'n', 'k', 'bytes per item'), loc='center')
-    plt.title(f'Top {t} Optimal $(m,n,k,z)$\nfor $f\leq{str(max_f).lstrip("0")}$ and $n\geq{min_n}$', pad=20)
-    fig.savefig(join(out_dir,'optimals_table.png'), bbox_inches='tight', dpi=300)
+    plt.title(f'Top {t} Optimal $(m,n,k,z)$\nfor $f\leq{f_max(max_f).lstrip("0")}$ and $n\geq{min_n}$', pad=20)
+    fig.savefig(join(out_dir,'optimals_table.pdf'), bbox_inches='tight')
     plt.close()
 
 if __name__ == '__main__':
